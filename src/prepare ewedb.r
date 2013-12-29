fbd9022a31ae30685c14a172b46314a1504bbdac
ivanhanigan <ivanhanigan@gmail.com>
2013-12-29 15:07:20 +1100

    add django v of src files

---
+++ src/prepare ewedb.r
@@ -0,0 +1,13 @@
+
+###########################################################################
+# newnode: prepare ewedb
+psql -h 115.146.94.209 -d ewedb -U postgres
+CREATE ROLE public_group;
+CREATE SCHEMA bom_grids;
+grant usage on schema bom_grids to public_group;
+CREATE ROLE ivan_hanigan LOGIN PASSWORD 'XXXX';
+GRANT ALL ON SCHEMA bom_grids to ivan_hanigan;
+\q
+# add to pg_hba
+reload
+select pg_reload_conf();
